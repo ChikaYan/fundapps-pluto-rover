83c0ed958344bd2756c5db626c33eb0e4578951d
Walter Wu <walterwuyan@gmail.com>
2019-01-23 14:17:48 +0000

    Fixed issues with first test

--- tests/pluto_rover_tests.py
+++ tests/pluto_rover_tests.py
@@ -1,8 +1,11 @@
+import sys
+sys.path.append("./../")
 from pluto_rover.pluto_rover import Rover
 from pluto_rover.pluto_rover import Dir
+
 
 
 def test_rover_can_move_n():
     rover = Rover(0, 0, Dir.N)
     rover.move("F")
-    assert rover.location == [0, 1, Dir.N]
+    assert rover.location() == [0, 1, Dir.N]
