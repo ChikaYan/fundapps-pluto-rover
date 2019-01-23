65a19306cc7031269016049549d52a5eae888e56
Walter Wu <walterwuyan@gmail.com>
2019-01-23 14:23:47 +0000

    Added test on b/f moves in all directions

--- tests/pluto_rover_tests.py
+++ tests/pluto_rover_tests.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("./../")
 from pluto_rover.pluto_rover import Rover
 from pluto_rover.pluto_rover import Dir
@@ -8,3 +9,21 @@ def test_rover_can_move_n():
     rover = Rover(0, 0, Dir.N)
     rover.move("F")
     assert rover.location() == [0, 1, Dir.N]
+
+
+def test_can_move_at_all_dirs():
+    rover = Rover(0, 0, Dir.N)
+    rover.move("FFFB")
+    assert rover.location() == [0, 2, Dir.N]
+
+    rover = Rover(0, 0, Dir.E)
+    rover.move("FFFB")
+    assert rover.location() == [2, 0, Dir.E]
+
+    rover = Rover(0, 0, Dir.S)
+    rover.move("BBBF")
+    assert rover.location() == [0, 2, Dir.S]
+
+    rover = Rover(0, 0, Dir.W)
+    rover.move("BBBF")
+    assert rover.location() == [2, 0, Dir.W]
