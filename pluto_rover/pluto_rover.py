6079bcfc34fc22f4491ae7eaf2d8ef010b8bc9fa
Walter Wu <walterwuyan@gmail.com>
2019-01-23 14:19:29 +0000

    Passed first test

--- pluto_rover/pluto_rover.py
+++ pluto_rover/pluto_rover.py
@@ -1,21 +1,33 @@
 from enum import Enum
 
 
-class Rover:
-    def __init__(self, x, y, dir):
-        self.x = x
-        self.y = y
-        self.dir = dir
-
-    def move(self, command):
-        pass
-
-    def location(self):
-        return
-
-
 class Dir(Enum):
     N = 1
     E = 2
     S = 3
     W = 4
+
+
+class Rover:
+    def __init__(self, x=0, y=0, dir=Dir.N):
+        self.x = x
+        self.y = y
+        self.dir = dir
+
+    def move(self, command):
+        for c in command:
+            if c == "F":
+                self.__forward()
+
+    def __forward(self):
+        if self.dir == Dir.N:
+            self.y += 1
+        elif self.dir == Dir.E:
+            pass
+        elif self.dir == Dir.S:
+            pass
+        else:
+            pass
+
+    def location(self):
+        return [self.x, self.y, self.dir]
--- tests/pluto_rover_tests.py
+++ tests/pluto_rover_tests.py
@@ -4,7 +4,6 @@ from pluto_rover.pluto_rover import Rover
 from pluto_rover.pluto_rover import Dir
 
 
-
 def test_rover_can_move_n():
     rover = Rover(0, 0, Dir.N)
     rover.move("F")
